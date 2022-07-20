c5810b2fd06482b8dadf64ce664eeb933e0b9ee6
Ivan Cao-Berg <icaoberg@psc.edu>
2022-07-19 21:29:12 -0400

    Added version 3.15.5

---
+++ 3.15.5/metaplasmidspades.py
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION=3.15.5
+PACKAGE=SPAdes
+TOOL=metaplasmidspades.py
+DIRECTORY=$(dirname $0)
+
+PERSISTENT_FILE_STORAGE=/ocean
+if [ -d $PERSISTENT_FILE_STORAGE ]; then
+	OPTIONS="-B $PERSISTENT_FILE_STORAGE"
+fi
+
+if [ -d /local ]; then
+	OPTIONS=$OPTIONS" -B /local"
+fi
+
+singularity exec -B $PERSISTENT_FILE_STORAGE $DIRECTORY/singularity-$PACKAGE-$VERSION.sif $TOOL "$@"
